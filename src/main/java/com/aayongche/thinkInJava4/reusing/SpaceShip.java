7db6309748d1ab968f1545307a8e2bc79baaf6fd
lyj <yanjun.liu@aayongche.com>
2016-03-29 15:53:36 +0800

    a

---
+++ src/main/java/com/aayongche/thinkInJava4/reusing/SpaceShip.java
@@ -0,0 +1,11 @@
+package com.aayongche.thinkInJava4.reusing;//: reusing/SpaceShip.java
+
+public class SpaceShip extends SpaceShipControls {
+  private String name;
+  public SpaceShip(String name) { this.name = name; }
+  public String toString() { return name; }
+  public static void main(String[] args) {
+    SpaceShip protector = new SpaceShip("NSEA Protector");
+    protector.forward(100);
+  }
+} ///:~
