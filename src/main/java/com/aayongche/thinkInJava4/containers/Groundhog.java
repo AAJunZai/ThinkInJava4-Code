b16ad1bd98b97085b71ff727da64dce2ad7b6eb3
lyj <yanjun.liu@aayongche.com>
2016-04-27 14:58:29 +0800

    aaa

--- src/main/java/com/aayongche/thinkInJava4/containers/Groundhog.java
+++ src/main/java/com/aayongche/thinkInJava4/containers/Groundhog.java
@@ -2,9 +2,13 @@ package com.aayongche.thinkInJava4.containers;//: containers/Groundhog.java
 // Looks plausible, but doesn't work as a HashMap key.
 
 public class Groundhog {
-  protected int number;
-  public Groundhog(int n) { number = n; }
-  public String toString() {
-    return "Groundhog #" + number;
-  }
+    protected int number;
+
+    public Groundhog(int n) {
+        number = n;
+    }
+
+    public String toString() {
+        return "Groundhog #" + number;
+    }
 } ///:~
