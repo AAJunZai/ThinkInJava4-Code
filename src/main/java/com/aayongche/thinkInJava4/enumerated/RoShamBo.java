5a7eb4572153e61b0cbb35b820faf4d47c0eea04
lyj <yanjun.liu@aayongche.com>
2016-05-10 14:13:47 +0800

    aa

--- src/main/java/com/aayongche/thinkInJava4/enumerated/RoShamBo.java
+++ src/main/java/com/aayongche/thinkInJava4/enumerated/RoShamBo.java
@@ -5,16 +5,13 @@ package com.aayongche.thinkInJava4.enumerated;
 import com.aayongche.thinkInJava4.net.mindview.util.Enums;
 
 public class RoShamBo {
-    public static <T extends Competitor<T>>
-    void match(T a, T b) {
-        System.out.println(
-                a + " vs. " + b + ": " + a.compete(b));
+    public static <T extends Competitor<T>> void match(T a, T b) {
+
+        System.out.println(a + " vs. " + b + ": " + a.compete(b));
     }
 
-    public static <T extends Enum<T> & Competitor<T>>
-    void play(Class<T> rsbClass, int size) {
+    public static <T extends Enum<T> & Competitor<T>> void play(Class<T> rsbClass, int size) {
         for (int i = 0; i < size; i++)
-            match(
-                    Enums.random(rsbClass), Enums.random(rsbClass));
+            match(Enums.random(rsbClass), Enums.random(rsbClass));
     }
 } ///:~
--- src/main/java/com/aayongche/thinkInJava4/enumerated/RoShamBo5.java
+++ src/main/java/com/aayongche/thinkInJava4/enumerated/RoShamBo5.java
@@ -7,23 +7,19 @@ import java.util.*;
 
 enum RoShamBo5 implements Competitor<RoShamBo5> {
     PAPER, SCISSORS, ROCK;
-    static EnumMap<RoShamBo5, EnumMap<RoShamBo5, Outcome>>
-            table = new EnumMap<RoShamBo5,
-            EnumMap<RoShamBo5, Outcome>>(RoShamBo5.class);
+    static EnumMap<RoShamBo5, EnumMap<RoShamBo5, Outcome>> table =
+            new EnumMap<RoShamBo5, EnumMap<RoShamBo5, Outcome>>(RoShamBo5.class);
 
     static {
         for (RoShamBo5 it : RoShamBo5.values())
-            table.put(it,
-                    new EnumMap<RoShamBo5, Outcome>(RoShamBo5.class));
+            table.put(it, new EnumMap<RoShamBo5, Outcome>(RoShamBo5.class));
         initRow(PAPER, Outcome.DRAW, Outcome.LOSE, Outcome.WIN);
         initRow(SCISSORS, Outcome.WIN, Outcome.DRAW, Outcome.LOSE);
         initRow(ROCK, Outcome.LOSE, Outcome.WIN, Outcome.DRAW);
     }
 
-    static void initRow(RoShamBo5 it,
-                        Outcome vPAPER, Outcome vSCISSORS, Outcome vROCK) {
-        EnumMap<RoShamBo5, Outcome> row =
-                RoShamBo5.table.get(it);
+    static void initRow(RoShamBo5 it, Outcome vPAPER, Outcome vSCISSORS, Outcome vROCK) {
+        EnumMap<RoShamBo5, Outcome> row = RoShamBo5.table.get(it);
         row.put(RoShamBo5.PAPER, vPAPER);
         row.put(RoShamBo5.SCISSORS, vSCISSORS);
         row.put(RoShamBo5.ROCK, vROCK);
