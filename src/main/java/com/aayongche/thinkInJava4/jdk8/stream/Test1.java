0fb3558709e8cd24cd1f0fb42eb902248f57adf2
AAJunZai <liuyanjunbj@163.com>
2016-06-29 16:34:49 +0800

    stream

--- src/main/java/com/aayongche/thinkInJava4/jdk8/stream/Test1.java
+++ src/main/java/com/aayongche/thinkInJava4/jdk8/stream/Test1.java
@@ -1,11 +1,9 @@
 package com.aayongche.thinkInJava4.jdk8.stream;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by lyj on 16-6-22.
@@ -13,10 +11,15 @@ import java.util.List;
 public class Test1 {
     public static void main(String[] args) throws IOException {
 
-        String contents=new String(Files.readAllBytes(Paths.get("/home/lyj/dos/workspace/java/ThinkInJava4-Code/src/main/java/com/aayongche/thinkInJava4/jdk8/stream/test1.txt")), StandardCharsets.UTF_8);
-        List<String> words= Arrays.asList(contents.split("[\\p{L}]+"));
-        long count=words.stream().filter(w->w.length()>1).count();
-        System.out.println(count);
+        List<String> words=new ArrayList<String>();
+        words.add("aaa");
+        words.add("qabc");
+        words.add("zzbbbbbb");
+        Optional<String> largest=words.stream().max(String::compareToIgnoreCase);
+        if(largest.isPresent()){
+            System.out.println("largest:"+largest.get());
+        }
 
+        largest.orElseGet(()->System.getProperty("user.dir"));
     }
 }
--- src/main/java/com/aayongche/thinkInJava4/jdk8/stream/Test2.java
+++ src/main/java/com/aayongche/thinkInJava4/jdk8/stream/Test2.java
@@ -1,19 +1,57 @@
 package com.aayongche.thinkInJava4.jdk8.stream;
 
 
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 /**
- *
  * Created by lyj on 16-6-22.
  */
 public class Test2 {
     public static void main(String[] args) {
+        //聚合操作
+       /* List<Integer> values = new ArrayList<Integer>();
+        values.add(1);
+        values.add(2);
+        values.add(3);
+        values.add(4);
+        values.add(5);
+        Stream<Integer> value = values.stream();
+        Integer sum = value.reduce(0, (x, y) -> x + y);
+        System.out.println(sum);*/
 
-        Stream<Double> randoms= Stream.generate(Math::random).limit(100);
-        Object[] obj=randoms.toArray();
-        System.out.println(Arrays.toString(obj));
+       /* List<String> values = new ArrayList<String>();
+        values.add("a");
+        values.add("ab");
+        values.add("abc");
+        values.add("abcd");
+        values.add("abcde");
+        Stream<String> value=values.stream();
+        int sum=value.reduce(0,(total,word)->total+word.length(),(total1,total2)->total1+total2);
+        System.out.println(sum);*/
+
+        //收集数据
+        /*List<String> values = new ArrayList<String>();
+        values.add("a");
+        values.add("ab");
+        values.add("abc");
+        values.add("abcd");
+        values.add("abcde");
+        Stream<String> value=values.stream();
+        String[] result=value.toArray(String[]::new);
+        System.out.println(Arrays.toString(result));*/
+
+        //收集到list set
+//        List<String> values = new ArrayList<String>();
+//        values.add("a");
+//        values.add("ab");
+//        values.add("abc");
+//        values.add("abcd");
+//        values.add("abcde");
+//        Stream<String> value=values.stream();
+//        value.forEach(System.out::println);
+//        List<String> result=value.collect(Collectors.toList());
+//        Set<String> set=value.collect(Collectors.toSet());
+//        String str=value.collect(Collectors.joining());
+//        String str1=value.collect(Collectors.joining(","));
 
 
     }
