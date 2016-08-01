746bade33fa18c29bf6b2530a37d932d6916180d
liuyanjunbj <liuyanjunbj@Gmail.com>
2016-08-01 14:28:00 +0800

    aa

---
+++ src/main/java/com/aayongche/thinkInJava4/jdk8/mixed/Test1.java
@@ -0,0 +1,15 @@
+package com.aayongche.thinkInJava4.jdk8.mixed;
+
+
+/**
+ * Created by Administrator on 2016/8/1.
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        String joined= String.join("/","aaa","bbb","ccc");
+        System.out.println(joined);
+
+        
+    }
+}
